a6a9bc2736adb7806dda0c3fb9e50b54ff7c18f1
Mukund Gohil <gohil.mukund@gmail.com>
2016-09-09 21:30:49 +0530

    SQL DB Helper Code updated

--- app/src/main/java/in/magikspark/deep/SplashActivity.java
+++ app/src/main/java/in/magikspark/deep/SplashActivity.java
@@ -1,15 +1,22 @@
 package in.magikspark.deep;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import in.magikspark.deep.data.LoginDbHelper;
 
 public class SplashActivity extends AppCompatActivity{
     private final int SPLASH_DISPLAY_LENGTH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LoginDbHelper mDbHelper = new LoginDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         new Handler().postDelayed(new Runnable(){
             @Override
